134d208768be66c40a04b76ea093fe8b61d32d6c
iguanastin <austinthompson194@gmail.com>
2019-09-04 21:11:18 -0600

    Throws IOException

--- src/main/java/menagerie/duplicates/DuplicateFinder.java
+++ src/main/java/menagerie/duplicates/DuplicateFinder.java
@@ -1,10 +1,11 @@
 package menagerie.duplicates;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public interface DuplicateFinder {
 
-    List<Match> getMatchesFor(File file);
+    List<Match> getMatchesFor(File file) throws IOException;
 
 }
